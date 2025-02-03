3ec2d1816107a285ae955c98bdec632225e6a883
Victor Scarano <victorascarano@gmail.com>
2025-02-02 21:36:55 -0600

    Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,87 @@
+{ config, pkgs, ... }:
+
+{
+	imports = [ ./hardware-configuration.nix ./home.nix ];
+
+	# enable flakes
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	nixpkgs.config.allowUnfree = true;
+
+	# system packages
+	programs.hyprland.enable = true;
+	environment.systemPackages = with pkgs; [ btop gcc gh git gparted libgcc polkit polkit_gnome stow ];
+
+	# shell config
+	programs.zsh.enable = true;
+	environment.shells = [ pkgs.zsh ];
+
+	# user config
+	users.users.victor = {
+		isNormalUser = true;
+		description = "Victor";
+		extraGroups = [ "networkmanager" "wheel" ];
+		shell = pkgs.zsh;
+		packages = with pkgs; [ discord-canary github-desktop spotify ];
+	};
+
+	boot.loader = {
+		systemd-boot = {
+			enable = true;
+			consoleMode = "max";
+		};
+		efi.canTouchEfiVariables = true;
+		timeout = 10;
+	};
+
+	# networking config
+	networking.networkmanager.enable = true;
+	networking.hostName = "victor-nixos"; 
+
+	# audio config
+	services.pulseaudio.enable = false;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+	};
+
+	# enable daemons and services
+	services.openssh.enable = true;
+	services.xserver.displayManager.gdm.enable = true;
+	services.printing.enable = true;
+
+	# polkit
+	security.polkit.enable = true;
+	systemd = {
+		user.services.polkit-gnome-authentication-agent-1 = {
+			description = "polkit-gnome-authentication-agent-1";
+			wantedBy = [ "graphical-session.target" ];
+			wants = [ "graphical-session.target" ];
+			after = [ "graphical-session.target" ];
+			serviceConfig = {
+				Type = "simple";
+				ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+				Restart = "on-failure";
+				RestartSec = 1;
+				TimeoutStopSec = 10;
+			};
+		};
+	};
+
+	# font config
+	fonts.packages = with pkgs; [
+		noto-fonts
+		noto-fonts-cjk-sans
+		nerd-fonts.jetbrains-mono
+		nerd-fonts.ubuntu-mono
+	];
+
+	# misc config
+	time.timeZone = "America/Chicago";
+	i18n.defaultLocale = "en_US.UTF-8";
+
+	# nixos release (https://nixos.org/nixos/options.html)
+	system.stateVersion = "24.11";
+}
---
+++ home.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+let
+	home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
+{
+	imports = [ (import "${home-manager}/nixos") ];
+
+	home-manager.users.victor = {
+		home = {
+			stateVersion = "24.11";
+			packages = with pkgs; [
+				anyrun
+				fastfetch
+				oh-my-posh
+				firefox
+				neovim
+				zsh
+			];
+		};
+		programs = {
+			ghostty = {
+				enable = true;
+				settings = {
+					theme = "vesper";
+					font-family = "UbuntuMono Nerd Font";
+					font-size = 23;
+					shell-integration = "zsh";
+					window-decoration = false;
+					background-opacity = 0.7;
+					mouse-hide-while-typing = true;
+				};
+			};
+			gh = {
+				enable = true;
+				gitCredentialHelper.enable = true;
+			};
+			git = {
+				enable = true;
+				userName = "Victor Scarano";
+				userEmail = "victorascarano@gmail.com";
+			};
+			kitty = {
+				enable = true;
+				font = {
+					name = "UbuntuMono Nerd Font";
+					size = 23;
+				};
+				settings = {
+					window_padding_width = 0;
+					# modify_font underline_position 2
+					background_opacity = 0.7;
+				};
+			};
+		};
+	};
+}
